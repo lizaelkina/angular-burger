bce9c9bca56e22825b4a68c93ccb184b58954d2e
Liza Elkina <lizapetkova@yandex.ru>
2023-11-16 14:58:49 +0300

    feat: подготовка структуры проекта

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+
+const routes: Routes = [
+  {path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule)},
+  {path: 'feed', loadChildren: () => import('./feed/feed.module').then(m => m.FeedModule)},
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule {
+}
---
+++ src/app/feed/feed.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FeedRoutingModule } from './feed-routing.module';
+import { FeedPageComponent } from './feed-page.component';
+
+
+@NgModule({
+  declarations: [
+    FeedPageComponent
+  ],
+  imports: [
+    CommonModule,
+    FeedRoutingModule
+  ]
+})
+export class FeedModule { }
