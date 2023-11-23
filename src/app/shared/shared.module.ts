2840247991a56d87baced8909a5253157af3fcef
Liza Elkina <lizapetkova@yandex.ru>
2023-11-23 16:54:59 +0300

    style: верстка компонента ingredient-details

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,11 +4,13 @@ import {HeaderComponent} from './components/header/header.component';
 import {RouterLink, RouterLinkActive} from '@angular/router';
 import {MatIconModule} from '@angular/material/icon';
 import {HttpClientModule} from '@angular/common/http';
+import { IngredientDetailsComponent } from './components/ingredient-details/ingredient-details.component';
 
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    IngredientDetailsComponent
   ],
   exports: [
     HeaderComponent
