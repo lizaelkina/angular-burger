bba35648a323640faf768344cf59bf6e6a350b18
Liza Elkina <lizapetkova@yandex.ru>
2023-11-30 19:36:49 +0300

    feat: сервис по получению ингредиентов с сервера

--- src/app/home/burger-ingredients/burger-ingredients.component.ts
+++ src/app/home/burger-ingredients/burger-ingredients.component.ts
@@ -1,10 +1,28 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {BurgerIngredientsService, Ingredient} from './burger-ingredients.service';
 
 @Component({
   selector: 'app-burger-ingredients',
   templateUrl: './burger-ingredients.component.html',
   styleUrls: ['./burger-ingredients.component.scss']
 })
-export class BurgerIngredientsComponent {
+export class BurgerIngredientsComponent implements OnInit {
 
+  ingredients: Ingredient[] = [];
+
+  constructor(private ingredientsService: BurgerIngredientsService) {
+  }
+
+  ngOnInit() {
+    this.getIngredients();
+  }
+
+  getIngredients() {
+    this.ingredientsService.getIngredients()
+      .subscribe({
+        next: ingredients => {
+          this.ingredients = ingredients
+        }
+      })
+  }
 }
